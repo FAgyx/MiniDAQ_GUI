060941915423ac1ca43602e82e29f0d6790a2083
yuxiang.g <yuxiang.g@cern.ch>
2020-06-18 23:51:08 -0400

    GUI functions added

--- UART_py3/TDCreg.py
+++ UART_py3/TDCreg.py
@@ -1,7 +1,6 @@
 from TDC_config_low_level_function import *
 
 
-
 class TDCreg(object):
     def __init__(self,ser):
         self.ser = ser
@@ -14,8 +13,7 @@ class TDCreg(object):
         self.control_1_align = ['0']
         trst_0(self.ser)
         trst_1(self.ser)
-        
-    
+
     def reset_setup_0(self):
         #setup_0
         #TTC_setup
@@ -56,7 +54,6 @@ class TDCreg(object):
         self.readout = [self.enable_trigger_timeout , self.enable_high_speed , self.enable_legacy , self.full_width_res , self.width_select , self.enable_8b10b , self.enable_insert , self.enable_error_packet , self.enable_TDC_ID , self.enable_error_notify]
         self.setup_0 = self.TTC_setup + self.bcr_distribute + self.tdc_mode + self.TDC_ID_l + self.readout
 
-
     def reset_setup_1(self):
         ##setup_1
         #timer_out
@@ -73,7 +70,6 @@ class TDCreg(object):
         self.timer_out = [self.combine_time_out_config , self.fake_hit_time_interval , self.syn_packet_number]
         self.coarse_out = [self.roll_over , self.coarse_count_offset , self.bunch_offset , self.event_offset , self.match_window]
         self.setup_1 = self.timer_out +self.coarse_out 
-
 
     def reset_setup_2(self):
         ##setup_2
@@ -98,13 +94,11 @@ class TDCreg(object):
         self.chnl_decode = [self.fine_sel , self.lut0 ,self.lut1 ,self.lut2, self.lut3 ,self.lut4 ,self.lut5 ,self.lut6 ,self.lut7 ,self.lut8 ,self.lut9 ,self.luta ,self.lutb ,self.lutc ,self.lutd ,self.lute ,self.lutf]
         self.setup_2 = self.chnl_decode
 
-
     def reset_setup(self):
         self.reset_setup_0()
         self.reset_setup_1()
         self.reset_setup_2()
         self.setup = [self.setup_0, self.setup_1, self.setup_2]
-
 
     def reset_control_0(self):
         ##control_0
@@ -118,7 +112,6 @@ class TDCreg(object):
         self.reset_control = [self.rst_ePLL , self.reset_jtag_in , self.event_reset_jtag_in , self.chnl_fifo_overflow_clear , self.debug_port_select]
         self.control_0 = self.reset_control
 
-
     def reset_control_1(self):
         ##control_1
         self.phase_clk160 = ['01000']
@@ -131,19 +124,16 @@ class TDCreg(object):
         self.ePLL_control = [self.phase_clk160 , self.phase_clk320_0 , self.phase_clk320_1 , self.phase_clk320_2 , self.ePllRes , self.ePllIcp , self.ePllCap,self.ePllRes , self.ePllIcp , self.ePllCap,self.ePllRes , self.ePllIcp , self.ePllCap]
         self.control_1 = self.ePLL_control
 
-
     def reset_conttrol(self):
         self.reset_control_0()
         self.reset_control_1()
         self.control = [self.control_0 , self.control_1]
-
 
     def reset_all_reg(self):
         trst_0(self.ser)
         trst_1(self.ser)
         self.reset_setup()
         self.reset_conttrol()
-
 
     def update_setup_0(self):
         self.setup_0_bin_str = ''.join(self.setup_0_align)
@@ -160,7 +150,6 @@ class TDCreg(object):
         start_action(self.ser)
         print("setup0 updated to TDC: "+hex(int(setup_0_bin_str,2)))
 
-
     def update_setup_1(self):
         self.setup_1_bin_str = ''.join(self.setup_1_align)
         for s in self.setup_1:
@@ -175,7 +164,6 @@ class TDCreg(object):
         update_bit_length(94,self.ser)
         start_action(self.ser)
         print("setup1 updated to TDC: "+hex(int(setup_1_bin_str,2)))
-
 
     def update_setup_2(self):
         self.setup_2_bin_str = ''.join(self.setup_2_align)
@@ -192,7 +180,6 @@ class TDCreg(object):
         start_action(self.ser)
         print("setup2 updated to TDC: "+hex(int(setup_2_bin_str,2)))
 
-
     def update_control_0(self):
         self.control_0_bin_str = ''.join(self.control_0_align)
         for s in self.control_0:
@@ -208,11 +195,10 @@ class TDCreg(object):
         start_action(self.ser)
         print("control_0 updated to TDC: "+hex(int(control_0_bin_str,2)))
 
-
     def update_control_1(self):
         self.control_1_bin_str = ''.join(self.control_1_align)
         for s in self.control_1:
-                self.control_1_bin_str = self.control_1_bin_str + ''.join(s)
+            self.control_1_bin_str = self.control_1_bin_str + ''.join(s)
         self.control_1_str=bin_to_hex(self.control_1_bin_str)
         update_reg(0,'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'+self.control_1_str,self.ser)
         update_reg(1,'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',self.ser)
@@ -224,13 +210,11 @@ class TDCreg(object):
         start_action(self.ser)
         print("control_1 updated to TDC: "+hex(int(control_1_bin_str,2)))
 
-
     def list_to_string(self,var):
         string_temp = ''
         for s in self.__dict__[var]:
             string_temp=string_temp+''.join(s)
         return string_temp
-
 
     def set_ePLL_phase(self,clk_160_phase,clk_320_phase):
         self.phase_clk160[0]=format(clk_160_phase%32,'b').zfill(5)
@@ -239,14 +223,12 @@ class TDCreg(object):
         self.phase_clk320_1[0]=format(clk_320_phase%16,'b').zfill(4)
         self.update_control_1()
 
-
-
     def reselect_dline(self,correct_counter=1000):
         tmp_r = self.channel_enable_r[0]
         tmp_f = self.channel_enable_f[0]
-        print('Phase clock160   = '+int(self.phase_clk160[0],2))
-        print('Phase clock320_0 = '+int(self.phase_clk320_0[0],2))
-        print('Phase clk320_1   = '+int(self.phase_clk320_1[0],2))
+        print('Phase clock160   = '+str(int(self.phase_clk160[0],2)))
+        print('Phase clock320_0 = '+str(int(self.phase_clk320_0[0],2)))
+        print('Phase clk320_1   = '+str(int(self.phase_clk320_1[0],2)))
 
         self.channel_enable_r[0] ='000000000000000000000000' # disable input to ensure 8b10b comma out
         self.channel_enable_f[0] ='000000000000000000000000'
@@ -261,14 +243,12 @@ class TDCreg(object):
         self.update_setup_0()
         print("Reselect deline done!")
 
-
     def run_320M_data_rate(self):
         self.enable_high_speed[0] = '1'
         self.enable_legacy[0] = '0'
         self.update_setup_0()
         tdc_high_speed(self.ser) 
         print("Set data rate to 320Mbps!")
-
 
     def run_160M_data_rate(self):
         self.enable_high_speed[0] = '0'
@@ -277,11 +257,9 @@ class TDCreg(object):
         tdc_low_speed(self.ser) 
         print("Set data rate to 160Mbps!")
 
-
     def run_80M_data_rate(self):
         self.enable_legacy[0] = '1'
         self.update_setup_0()
-
 
     def run_single_edge_mode(self):  # 24'b dataword = 5'b chnlID + 1'b0 + 1'b is_rising_edge + 17'b leading_edge
         self.channel_data_debug[0] = '0'
@@ -291,7 +269,6 @@ class TDCreg(object):
         self.wordbyte = 3
         update_data_length(self.ser, 3)
 
-
     def run_double_edge_mode(self):  # 40'b dataword = 5'b chnlID + 2'b10 + 17'b leading_edge + 16'b trailing_edge_LSB
         self.channel_data_debug[0] = '0'
         self.enable_leading[0] = '0'
@@ -299,7 +276,6 @@ class TDCreg(object):
         self.update_setup_0()
         self.wordbyte = 5
         update_data_length(self.ser, 5)
-
 
     def run_pair_mode(self,width_select=0):  # 32'b dataword = 5'b chnlID + 2'b11 + 17'b leading_edge + 8'b width
         self.channel_data_debug[0] = '0'
@@ -311,7 +287,6 @@ class TDCreg(object):
         self.wordbyte = 4
         update_data_length(self.ser, 4)
 
-
     def run_pair_full_width_mode(self):  # 40'b dataword = 5'b chnlID + 2'b11 + 17'b leading_edge + 16'b width
         self.channel_data_debug[0] = '0'
         self.enable_leading[0] = '0'
@@ -321,7 +296,6 @@ class TDCreg(object):
         self.wordbyte = 5
         update_data_length(self.ser, 5)
 
-
     def run_debug_mode(self):  # 24'b dataword0 = 2'b00 + 5'b chnl_ID + 17'b time
                                # 24'b dataword1 = 2'b11 + 1'b edge_mode + 2'b hit_counter + 15'b drop_coarse + 4'b fineQ
         self.channel_data_debug[0] = '1'
@@ -329,18 +303,15 @@ class TDCreg(object):
         self.wordbyte = 3
         update_data_length(self.ser, 3)
 
-
     def run_idle_insert_mode(self, syn_packet_number):
         self.enable_insert[0] = '1'
         self.update_setup_0()
         self.syn_packet_number[0] = "{:0>12b}".format(syn_packet_number%4096)
         self.update_setup_1()
 
-
     def run_error_insert_mode(self):
         self.enable_error_packet[0] = '1'
         self.update_setup_0()
-
 
     def run_TDCID_mode(self):
         self.enable_TDC_ID[0] = '1'
@@ -348,11 +319,9 @@ class TDCreg(object):
         self.wordbyte = 3
         update_data_length(self.ser, 3)
 
-
     def config_TDCID(self, TDCID):
         self.TDC_ID[0] = "{:0>19b}".format(TDCID)
         self.update_setup_0()
-
 
     def disable_8b10b(self):
         self.enable_8b10b[0] = '0'
@@ -400,21 +369,17 @@ class TDCreg(object):
         enable_ttc_event_reset(self.ser)
         single_TTC(self.ser)
 
-
     def single_BCR(self):
         enable_ttc_BCR(self.ser)
         single_TTC(self.ser)
-
 
     def master_reset(self):
         enable_ttc_master_reset(self.ser)
         single_TTC(self.ser)
 
-
     def reset_JTAG(self):
         trst_0(self.ser)
         trst_1(self.ser)
-
 
     def update_JTAG(self):
         update_setup_0(self.ser)
@@ -423,14 +388,13 @@ class TDCreg(object):
         update_control_0(self.ser)
         update_control_1(self.ser)
 
-
     def set_FPGA_sample_rate(self):
         if self.enable_legacy[0] == '1':
             print("Set sample rate to 80Mbps!")
-        else if self.enable_high_speed[0] == '1':
+        elif self.enable_high_speed[0] == '1':
             tdc_high_speed(self.ser)
             print("Set sample rate to 320Mbps!")
-        else if self.enable_high_speed[0] == '0':
+        elif self.enable_high_speed[0] == '0':
             tdc_low_speed(self.ser)
             print("Set sample rate to 160Mbps!")
 
@@ -439,17 +403,17 @@ class TDCreg(object):
             self.wordbyte = 3             # TDC_ID mode
             print("Running in TDC_ID mode, TDC_ID=0X_"+
             format(int(self.TDC_ID[0],2),'05X'))
-        else if self.channel_data_debug[0] == '1':
+        elif self.channel_data_debug[0] == '1':
             self.wordbyte = 3             # debug mode
             print("Running in debug mode")
-        else if self.enable_leading[0] == '1':
+        elif self.enable_leading[0] == '1':
             self.wordbyte = 3             # single edge mode 
             print("Running in single edge mode")    
-        else if self.enable_leading[0] == '0':
+        elif self.enable_leading[0] == '0':
             if self.enable_pair[0] == '0': # double edge mode
                 self.wordbyte = 5
                 print("Running in double edge mode")
-            else if self.enable_pair[0] == '1':
+            elif self.enable_pair[0] == '1':
                 if self.full_width_res[0] == '0': # pair mode
                     self.wordbyte = 4
                     print("Running in pair mode")
@@ -461,12 +425,11 @@ class TDCreg(object):
                 ' ns ' + format(int(self.combine_time_out_config[0],2),'03X') +' LSB=6.25ns')
         update_data_length(self.ser, self.wordbyte)
 
-
     def set_trigger_type(self):
         if self.enable_trigger[0] == '0':  # triggerless mode
             tdc_triggerless_mode(self.ser)
             print("Running in triggerless mode")
-        else if self.enable_trigger[0] == '1':  # triggered mode
+        elif self.enable_trigger[0] == '1':  # triggered mode
             print("Running in triggered mode")
             if self.enbale_fake_hit[0] == '1':
                 print("Fake hit enabled, interval = "+
@@ -481,7 +444,6 @@ class TDCreg(object):
             if self.enable_trigger_timeout[0] == '1':
                 print("Trigger time out enabled")
 
-
     def show_channel_info(self):
         print("Rising is leading: 0X_"+format(int(self.rising_is_leading[0],2),'06X'))
         print([i for i in range(24) if rising_is_leading[0][i]=='1'])
@@ -489,7 +451,6 @@ class TDCreg(object):
         print([i for i in range(24) if rising_is_leading[0][i]=='1'])
         print("Channel enable F : 0X_"+format(int(self.channel_enable_f[0],2),'06X'))
         print([i for i in range(24) if rising_is_leading[0][i]=='1'])
-
 
     def show_packet_info(self):
         if self.enable_8b10b[0] == '1':
@@ -511,13 +472,12 @@ class TDCreg(object):
         ttc_str = ''
         ttc_str += 'New' if self.enable_new_ttc[0] == '1' else 'Legacy'
         ttc_str += ' TTC enabled for '
-        ttc_str += 'Master_reset ' if self.enable_master_reset_code[0] == '1'
-        ttc_str += 'BC_reset ' if self.auto_roll_over[0] == '0' 
-        and enable_direct_bunch_reset[0] == '0'
-        ttc_str += 'Trigger ' if self.enable_direct_trigger[0] == '0'
-        ttc_str += 'Event_reset' if self.enable_direct_event_reset[0] == '0'
+        ttc_str += 'Master_reset ' if self.enable_master_reset_code[0] == '1' else ''
+        ttc_str += 'BC_reset ' if self.auto_roll_over[0] == '0' \
+        and enable_direct_bunch_reset[0] == '0' else ''
+        ttc_str += 'Trigger ' if self.enable_direct_trigger[0] == '0' else ''
+        ttc_str += 'Event_reset' if self.enable_direct_event_reset[0] == '0' else ''
         print(ttc_str)
-
 
     def show_BCR_info(self):
         bcr_str = ''
@@ -528,11 +488,9 @@ class TDCreg(object):
             if enable_direct_bunch_reset[0] == '0':
                 bcr_str += 'TTC ' 
         bcr_str += 'with roll_over = 0X_'+format(int(self.roll_over[0],2),'03X')+' BC '
-        bcr_str += 'and delay = 0X_'+format(int(self.coarse_count_offset[0],2),'03X')
+        bcr_str += 'and delay = 0X_'+format(int(self.coarse_count_offset[0],2),'03X') \
         +' BC' if self.bypass_bcr_distribution[0] == '0' else 'and no delay'
         print(bcr_str)
-
-
 
     def DAQ_init(self):
         print("//////DAQ starts!//////")
