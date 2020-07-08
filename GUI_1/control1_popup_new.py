9a24efa9ab07de071ef994642598a528fd254348
yuxiang.g <yuxiang.g@cern.ch>
2020-07-08 15:54:30 -0400

    Update control1_popup_new.py

--- GUI_1/control1_popup_new.py
+++ GUI_1/control1_popup_new.py
@@ -6,7 +6,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-# adding changes
+# adding changes 1
 ############## control1 popup script #################
 
 from PyQt5 import QtCore, QtGui, QtWidgets
