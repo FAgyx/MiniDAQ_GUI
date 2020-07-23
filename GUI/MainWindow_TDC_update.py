8bd52032d7df55aa1094b6b060532d1ce6679286
MaryKBoss <marybossard159@gmail.com>
2020-07-23 10:17:31 -0400

    creating/reading xml file
    Hi Yuxiang! Here, the file 'create_TDC_default.py' creates the xml file 'TDC_default.xml'. The values set in the xml file are loaded into the GUI when the 'load setup' button is pressed! I put in variables different from the ones set in TDCreg.py, so that the changes can be seen in the GUI. So far, when the init button is pressed, then all the new values are run!

--- GUI/MainWindow_TDC_update.py
+++ GUI/MainWindow_TDC_update.py
@@ -6,14 +6,13 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 ####
 import xml.etree.ElementTree as ET
-tree = ET.parse('TDC_config_default.xml')
+#tree = ET.parse('TDC_config_default.xml')
+tree = ET.parse('TDC_default.xml')
 root = tree.getroot()
-#print(root[0][0].text)
 ###
 from PyQt5 import *
 from PyQt5 import QtWidgets
@@ -24,13 +23,6 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 
 # Importing the popup windows
-# from verificate_popup import Ui_Dialog as Form
-# from ePLL_popup import Ui_Dialog as Form_2
-# from datarate_popup import Ui_Dialog as Form_3
-# from runmode_popup import Ui_Dialog as Form_4
-# from wordmode_popup import Ui_Dialog as Form_5
-# from edgetype_popup import Ui_Dialog as Form_6
-# from channel_popup import Ui_Dialog as Form_7
 from setup_0_popup import Ui_Dialog as Form_8
 from setup_1_popup import Ui_Dialog as Form_9
 from setup_2_popup import Ui_Dialog as Form_10
@@ -102,13 +94,28 @@ class Ui_MainWindow(object):
         self.comboBox.addItem("")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
+        if self.TDC_inst.enable_high_speed[0] == '1':
+            self.comboBox.setCurrentIndex(0)
+            self.TDC_inst.run_320M_data_rate()
+        if self.TDC_inst.enable_high_speed[0] == '0':
+            self.comboBox.setCurrentIndex(1)
+            self.TDC_inst.run_160M_data_rate()
+        if self.TDC_inst.enable_legacy[0] == '1':
+            self.comboBox.setCurrentIndex(2)
+            self.TDC_inst.run_80M_data_rate()
         self.comboBox.activated.connect(self.data_rate)
+
+
 
         self.gridLayout.addWidget(self.comboBox, 0, 1, 1, 1)
         self.comboBox_2 = QtWidgets.QComboBox(self.gridLayoutWidget)
         self.comboBox_2.setObjectName("comboBox_2")
         self.comboBox_2.addItem("")
         self.comboBox_2.addItem("")
+        if self.TDC_inst.enable_trigger[0] == '0':
+            self.comboBox_2.setCurrentIndex(0)
+        if self.TDC_inst.enable_trigger[0] == '1':
+            self.comboBox_2.setCurrentIndex(1)
         self.comboBox_2.activated.connect(self.trigger_mode)
 
         self.gridLayout.addWidget(self.comboBox_2, 1, 1, 1, 1)
@@ -269,7 +276,7 @@ class Ui_MainWindow(object):
         self.comboBox_3.setItemText(3, _translate("MainWindow", "pair_full_width"))
         self.comboBox_3.setItemText(4, _translate("MainWindow", "debug"))
         self.comboBox_3.setItemText(5, _translate("MainWindow", "TDC_ID"))
-        self.comboBox_4.setItemText(0, _translate("MainWindow", "leading"))
+        self.comboBox_4.setItemText(0, _translate("MainWindow", "rising"))
         self.comboBox_4.setItemText(1, _translate("MainWindow", "falling"))
         self.label_3.setText(_translate("MainWindow", "Format:"))
         self.label_5.setText(_translate("MainWindow", "Clk160_phase: "))
@@ -421,10 +428,10 @@ class Ui_MainWindow(object):
             print("master reset 0")
 
 
-    ### connecting to the TDC xml file
+    ### connecting to the TDC xml default file
     def load_setup_func(self):
         print("TDC_config_default loaded")
-        #print("enable_new_ttc: " + root[0][0].text)
+        #mode set
         self.TDC_inst.enable_new_ttc[0] = root[0][0].text
         self.TDC_inst.enable_master_reset_code[0] = root[0][1].text
         self.TDC_inst.enable_direct_bunch_reset[0] = root[0][2].text
@@ -445,6 +452,111 @@ class Ui_MainWindow(object):
         self.TDC_inst.enable_error_packet[0] = root[0][17].text
         self.TDC_inst.enable_TDC_ID[0] = root[0][18].text
         self.TDC_inst.enable_error_notify[0] = root[0][19].text
+
+        #internal_counter
+        self.TDC_inst.combine_time_out_config[0] = root[1][0].text
+        self.TDC_inst.fake_hit_time_interval[0] = root[1][1].text
+        self.TDC_inst.syn_packet_number[0] = root[1][2].text
+        self.TDC_inst.roll_over[0] = root[1][3].text
+        self.TDC_inst.coarse_count_offset[0] = root[1][4].text
+        self.TDC_inst.bunch_offset[0] = root[1][5].text
+        self.TDC_inst.event_offset[0] = root[1][6].text
+        self.TDC_inst.match_window[0] = root[1][7].text
+
+        #fine_time_lut
+        self.TDC_inst.fine_sel[0] = root[2][0].text
+        self.TDC_inst.lut0[0] = root[2][1].text
+        self.TDC_inst.lut1[0] = root[2][2].text
+        self.TDC_inst.lut2[0] = root[2][3].text
+        self.TDC_inst.lut3[0] = root[2][4].text
+        self.TDC_inst.lut4[0] = root[2][5].text
+        self.TDC_inst.lut5[0] = root[2][6].text
+        self.TDC_inst.lut6[0] = root[2][7].text
+        self.TDC_inst.lut7[0] = root[2][8].text
+        self.TDC_inst.lut8[0] = root[2][9].text
+        self.TDC_inst.lut9[0] = root[2][10].text
+        self.TDC_inst.luta[0] = root[2][11].text
+        self.TDC_inst.lutb[0] = root[2][12].text
+        self.TDC_inst.lutc[0] = root[2][13].text
+        self.TDC_inst.lutd[0] = root[2][14].text
+        self.TDC_inst.lute[0] = root[2][15].text
+        self.TDC_inst.lutf[0] = root[2][16].text
+
+        #reset_option
+        self.TDC_inst.rst_ePLL[0] = root[3][0].text
+        self.TDC_inst.reset_jtag_in[0] = root[3][1].text
+        self.TDC_inst.event_reset_jtag_in[0] = root[3][2].text
+        self.TDC_inst.chnl_fifo_overflow_clear[0] = root[3][3].text
+        self.TDC_inst.debug_port_select[0] = root[3][4].text
+
+        #ePll_option
+        self.TDC_inst.phase_clk160[0] = root[4][0].text
+        self.TDC_inst.phase_clk320_0[0] = root[4][1].text
+        self.TDC_inst.phase_clk320_1[0] = root[4][2].text
+        self.TDC_inst.phase_clk320_2[0] = root[4][3].text
+        self.TDC_inst.ePllRes[0] = root[4][4].text
+        self.TDC_inst.ePllIcp[0] = root[4][5].text
+        self.TDC_inst.ePllCap[0] = root[4][6].text
+
+        #data_rate
+        if root[6][0].text == '1':
+            self.comboBox.setCurrentIndex(0)
+            self.TDC_inst.run_320M_data_rate()
+        if root[6][1].text == '1':
+            self.comboBox.setCurrentIndex(1)
+            self.TDC_inst.run_160M_data_rate()
+        if root[6][2].text == '1':
+            self.comboBox.setCurrentIndex(2)
+            self.TDC_inst.run_80M_data_rate()
+
+        #trigger_mode
+        if root[7][0].text == '0':
+            self.comboBox_2.setCurrentIndex(0)
+            self.TDC_inst.run_triggerless_mode()
+        if root[7][0].text == '1':
+            self.comboBox_2.setCurrentIndex(1)
+            self.TDC_inst.run_trigger_mode()
+
+        #format
+        if root[8][0].text == '1':
+            self.comboBox_3.setCurrentIndex(0)
+            self.TDC_inst.run_pair_mode()
+        if root[8][1].text == '1':
+            self.comboBox_3.setCurrentIndex(1)
+            self.TDC_inst.run_single_edge_mode()
+        if root[8][2].text == '1':
+            self.comboBox_3.setCurrentIndex(2)
+            self.TDC_inst.run_double_edge_mode()
+        if root[8][3].text == '1':
+            self.comboBox_3.setCurrentIndex(3)
+            self.TDC_inst.run_pair_full_width_mode()
+        if root[8][4].text == '1':
+            self.comboBox_3.setCurrentIndex(4)
+            self.TDC_inst.run_debug_mode()
+        if root[8][5].text == '1':
+            self.comboBox_3.setCurrentIndex(5)
+            self.TDC_inst.run_TDCID_mode()
+
+        #edge_type
+        if root[9][0].text == '0':
+            self.comboBox_4.setCurrentIndex(0)
+            self.TDC_inst.set_rising_is_leading()
+        if root[9][0].text == '1':
+            self.comboBox_4.setCurrentIndex(1)
+            self.TDC_inst.set_falling_is_leading()
+
+        #clk_160_phase
+        self.spinBox.setValue(int(root[10][0].text))
+        self.phase_clk160_binary = format(self.spinBox.value(), '05b')
+        self.TDC_inst.phase_clk160[0] = self.phase_clk160_binary
+
+        #clk_320_phase
+        self.spinBox_2.setValue(int(root[11][0].text))
+        self.phase_clk320_0_binary = format(self.spinBox_2.value() + 4, '04b')[-4:]
+        self.TDC_inst.phase_clk320_0[0] = self.phase_clk320_0_binary
+
+        self.phase_clk320_1_binary = format(self.spinBox_2.value(), '04b')
+        self.TDC_inst.phase_clk320_1[0] = self.phase_clk320_1_binary
 
 
 if __name__ == "__main__":
---
+++ GUI/create_TDC_defualt.py
@@ -0,0 +1,191 @@
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+
+def generateXML(fileName):
+	configuration = ET.Element('configuration')
+
+	#mode_set variables 
+	mode_set = ET.SubElement(configuration, 'mode_set')
+	enable_new_ttc = ET.SubElement(mode_set, 'enable_new_ttc')
+	enable_new_ttc.text = '0'
+	enable_master_reset_code = ET.SubElement(mode_set, 'enable_master_reset_code')
+	enable_master_reset_code.text = '0'
+	enable_direct_bunch_reset = ET.SubElement(mode_set, 'enable_direct_bunch_reset')
+	enable_direct_bunch_reset.text = '0'
+	enable_direct_trigger = ET.SubElement(mode_set, 'enable_direct_trigger')
+	enable_direct_trigger.text = '0'
+	auto_roll_over = ET.SubElement(mode_set, 'auto_roll_over')
+	auto_roll_over.text = '1'
+	bypass_bcr_distribution = ET.SubElement(mode_set, 'bypass_bcr_distribution')
+	bypass_bcr_distribution.text = '0'
+	enable_trigger = ET.SubElement(mode_set, 'enable_trigger')
+	enable_trigger.text = '0'
+	channel_data_debug = ET.SubElement(mode_set, 'channel_data_debug')
+	channel_data_debug.text = '0'
+	enable_leading = ET.SubElement(mode_set, 'enable_leading')
+	enable_leading.text = '0'
+	enable_pair = ET.SubElement(mode_set, 'enable_pair')
+	enable_pair.text = '1'
+	enable_fake_hit = ET.SubElement(mode_set, 'enable_fake_hit')
+	enable_fake_hit.text = '0'
+	enable_trigger_timeout = ET.SubElement(mode_set, 'enable_trigger_timeout')
+	enable_trigger_timeout.text = '0'
+	enable_high_speed = ET.SubElement(mode_set, 'enable_high_speed')
+	enable_high_speed.text = '1'
+	enable_legacy = ET.SubElement(mode_set, 'enable_legacy')
+	enable_legacy.text = '0'
+	full_width_res = ET.SubElement(mode_set, 'full_width_res')
+	full_width_res.text = '0'
+	enable_8b10b = ET.SubElement(mode_set, 'enable_8b10b')
+	enable_8b10b.text = '1'
+	enable_insert = ET.SubElement(mode_set, 'enable_insert')
+	enable_insert.text = '0'
+	enable_error_packet = ET.SubElement(mode_set, 'enable_error_packet')
+	enable_error_packet.text = '0'
+	enable_TDC_ID = ET.SubElement(mode_set, 'enable_TDC_ID')
+	enable_TDC_ID.text = '0'
+	enable_error_notify = ET.SubElement(mode_set, 'enable_error_notify')
+	enable_error_notify.text = '0'
+
+	#internal_counter variables 
+	internal_counter = ET.SubElement(configuration, 'internal_counter')
+	combine_time_out_config = ET.SubElement(internal_counter, 'combine_time_out_config')
+	combine_time_out_config.text = '0000101000'
+	fake_hit_time_interval = ET.SubElement(internal_counter, 'fake_hit_time_interval')
+	fake_hit_time_interval.text = '000100000000'
+	syn_packet_number = ET.SubElement(internal_counter, 'syn_packet_number')
+	syn_packet_number.text = '111111111111'
+	roll_over = ET.SubElement(internal_counter, 'roll_over')
+	roll_over.text = '111111111111'
+	coarse_count_offset = ET.SubElement(internal_counter, 'coarse_count_offset')
+	coarse_count_offset.text = '000000000000'
+	bunch_offset = ET.SubElement(internal_counter, 'bunch_offset')
+	bunch_offset.text = '111110011100'
+	event_offset = ET.SubElement(internal_counter, 'event_offset')
+	event_offset.text = '000000000000'
+	match_window = ET.SubElement(internal_counter, 'match_window')
+	match_window.text = '000000011111'
+
+	#fine_time_lut variables 
+	fine_time_lut = ET.SubElement(configuration, 'fine_time_lut')
+	fine_sel = ET.SubElement(fine_time_lut, 'fine_sel')
+	fine_sel.text = '0011'
+	lut0 = ET.SubElement(fine_time_lut, 'lut0')
+	lut0.text = '00'
+	lut1 = ET.SubElement(fine_time_lut, 'lut1')
+	lut1.text = '01'
+	lut2 = ET.SubElement(fine_time_lut, 'lut2')
+	lut2.text = '10'
+	lut3 = ET.SubElement(fine_time_lut, 'lut3')
+	lut3.text = '01'
+	lut4 = ET.SubElement(fine_time_lut, 'lut4')
+	lut4.text = '11'
+	lut5 = ET.SubElement(fine_time_lut, 'lut5')
+	lut5.text = '00'
+	lut6 = ET.SubElement(fine_time_lut, 'lut6')
+	lut6.text = '10'
+	lut7 = ET.SubElement(fine_time_lut, 'lut7')
+	lut7.text = '10'
+	lut8 = ET.SubElement(fine_time_lut, 'lut8')
+	lut8.text = '00'
+	lut9 = ET.SubElement(fine_time_lut, 'lut9')
+	lut9.text = '00'
+	luta = ET.SubElement(fine_time_lut, 'luta')
+	luta.text = '00'
+	lutb = ET.SubElement(fine_time_lut, 'lutb')
+	lutb.text = '01'
+	lutc = ET.SubElement(fine_time_lut, 'lutc')
+	lutc.text = '11'
+	lutd = ET.SubElement(fine_time_lut, 'lutd')
+	lutd.text = '00'
+	lute = ET.SubElement(fine_time_lut, 'lute')
+	lute.text = '11'
+	lutf = ET.SubElement(fine_time_lut, 'lutf')
+	lutf.text = '00'
+
+	#reset_option variables 
+	reset_option = ET.SubElement(configuration, 'reset_option')
+	rst_ePLL = ET.SubElement(reset_option, 'rst_ePLL')
+	rst_ePLL.text = '0'
+	reset_jtag_in = ET.SubElement(reset_option, 'reset_jtag_in')
+	reset_jtag_in.text = '0'
+	event_reset_jtag_in = ET.SubElement(reset_option, 'event_reset_jtag_in')
+	event_reset_jtag_in.text = '0'
+	chnl_fifo_overflow_clear = ET.SubElement(reset_option, 'chnl_fifo_overflow_clear')
+	chnl_fifo_overflow_clear.text = '0'
+	debug_port_select = ET.SubElement(reset_option, 'debug_port_select')
+	debug_port_select.text = '0000'
+
+	#ePll_option variables 
+	ePll_option = ET.SubElement(configuration, 'ePll_option')
+	phase_clk160 = ET.SubElement(ePll_option, 'phase_clk160')
+	phase_clk160.text = '01000'
+	phase_clk320_0 = ET.SubElement(ePll_option, 'phase_clk320_0')
+	phase_clk320_0.text = '0100'
+	phase_clk320_1 = ET.SubElement(ePll_option, 'phase_clk320_1')
+	phase_clk320_1.text = '0000'
+	phase_clk320_2 = ET.SubElement(ePll_option, 'phase_clk320_2')
+	phase_clk320_2.text = '0010'
+	ePllRes = ET.SubElement(ePll_option, 'ePllRes')
+	ePllRes.text = '0010'
+	ePllIcp = ET.SubElement(ePll_option, 'ePllIcp')
+	ePllIcp.text = '0100'
+	ePllCap = ET.SubElement(ePll_option, 'ePllCap')
+	ePllCap.text = '10'
+
+	#TDC_status variables 
+	TDC_status = ET.SubElement(configuration, 'TDC_status')
+
+	#data_rate variables (1:run at that rate)
+	data_rate = ET.SubElement(configuration, 'data_rate')
+	run_320_rate = ET.SubElement(data_rate, 'run_320_rate')
+	run_320_rate.text = '0'
+	run_160_rate = ET.SubElement(data_rate, 'run_160_rate')
+	run_160_rate.text = '1'
+	run_80_rate = ET.SubElement(data_rate, 'run_80_rate')
+	run_80_rate.text = '0'
+
+
+	#trigger_mode variables (0: triggerless mode , 1: triggered mode)
+	trigger_mode = ET.SubElement(configuration, 'trigger_mode')
+	enable_trigger = ET.SubElement(trigger_mode, 'enable_trigger')
+	enable_trigger.text = '1'
+
+	#format variables   (choose one mode to run in, 1:run in that mode)
+	formatting = ET.SubElement(configuration, 'formatting')
+	pair_mode = ET.SubElement(formatting, 'pair_mode')
+	pair_mode.text = '0'
+	edge_mode = ET.SubElement(formatting, 'edge_mode')
+	edge_mode.text = '1'
+	double_edge_mode = ET.SubElement(formatting, 'double_edge_mode')
+	double_edge_mode.text = '0'
+	pair_full_width_mode = ET.SubElement(formatting, 'pair_full_width_mode')
+	pair_full_width_mode.text = '0'
+	debug_mode = ET.SubElement(formatting, 'debug_mode')
+	debug_mode.text = '0'
+	TDC_ID_mode = ET.SubElement(formatting, 'TDC_ID_mode')
+	TDC_ID_mode.text = '0'
+
+	#edge_type variables (0:rising_is_leading, 1:falling_is_leading)
+	edge_type = ET.SubElement(configuration, 'edge_type')
+	leading_type = ET.SubElement(edge_type, 'leading_type')
+	leading_type.text = '1'
+
+	#phase_clk160
+	phase_clk160 = ET.SubElement(configuration, 'phase_clk160')
+	phase_clk160_value = ET.SubElement(phase_clk160, 'phase_clk160_value')
+	phase_clk160_value.text = '7'
+
+	#phase_clk320_1
+	phase_clk320_1 = ET.SubElement(configuration, 'phase_clk320_1')
+	phase_clk320_1_value = ET.SubElement(phase_clk320_1, 'phase_clk320_1_value')
+	phase_clk320_1_value.text = '4'
+
+
+	#create a new XML file with correct indentations
+	xmlstr = minidom.parseString(ET.tostring(configuration)).toprettyxml(indent="	")
+	with open(fileName, "w") as f:
+		f.write(xmlstr)
+
+if __name__ == "__main__":
+	generateXML("TDC_default.xml")
